f75b1fcb619fa249952e8f7209864ea7b8ec629d
Michael Rivera <146748687+MikeMan161@users.noreply.github.com>
2026-05-03 18:58:54 -0400

    Update baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "MikeMan161.github.io/dev-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
